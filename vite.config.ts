f430f08d33e64ec4e707fa20e8bbfab9ec63e745
Milkyway <milkyway@Mac.mshome.net>
2026-01-07 16:05:24 +0900

    0107 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/api/tronscan'으로 시작하는 요청은 실제 TronScan 서버로 보냄
+      // '/api/tronscan'으로 시작하는 요청을 만나면 target으로 토스한다
       '/api/tronscan': {
         target: 'https://apilist.tronscanapi.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/tronscan/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api\/tronscan/, ''),
+        secure: false,
+      },
+    },
+  },
+});
